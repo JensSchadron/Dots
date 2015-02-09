9951984b042f930a3e7da8fb5737ab8f94b7e55c
alexander <alexanderkestens@gmail.com>
2015-02-09 13:23:11 +0100

    test

--- Dots/src/be/kdg/dots/model/DotKleur.java
+++ Dots/src/be/kdg/dots/model/DotKleur.java
@@ -5,6 +5,7 @@ import java.util.Random;
 /**
  * Created by alexander on 4/02/2015.
  */
+//TODO: hallo jens
 public enum DotKleur {
     BLUE(127,26,243),RED(245,0,82),GREEN(0,245,122),YELLOW(255,255,0),PURPLE(255,0,255);
 
