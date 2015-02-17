295d212d0f47304db4692187ed6a8003336ac285
alexander <alexanderkestens@gmail.com>
2015-02-17 19:33:50 +0100

    17/02/2015 19:33 knop op glassPane + icons op het spel

--- Dots/src/be/kdg/dots/view/GUIGlassPane.java
+++ Dots/src/be/kdg/dots/view/GUIGlassPane.java
@@ -2,38 +2,47 @@ package be.kdg.dots.view;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * Created by alexander on 17/02/2015.
  */
-public class GUIGlassPane extends JPanel implements ActionListener {
+public class GUIGlassPane extends JPanel{
     private Container contentPane;
     private boolean inited;
+    private JButton btnSave;
 
     public GUIGlassPane(Container contentPane) {
         this.contentPane = contentPane;
         setLayout(null);
+        MakeComponents();
+        MakeLayout();
+        MakeEventListener();
+    }
+
+    private void MakeComponents() {
+        btnSave = new JButton("Save settings");
+    }
+    private void MakeLayout() {
+        btnSave.setForeground(Color.BLUE);
+        add(btnSave);
+        btnSave.setBounds(40, getHeight() / 2, 350, 30);
 
     }
 
-    private void init() {
+    /*private void init() {
         inited = true;
         JLabel label = new JLabel("test2:");
         label.setForeground(Color.blue);
         add(label);
         label.setBounds(40, getHeight()/2, 350, 20);
 
-    }
+    }*/
 
     @Override
     public void paintComponent(Graphics gr) {
         super.paintComponent(gr);
-
-        if(!inited) {
-            init();
-        }
 
         Graphics2D g = (Graphics2D) gr;
 
@@ -42,7 +51,6 @@ public class GUIGlassPane extends JPanel implements ActionListener {
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING , RenderingHints.VALUE_ANTIALIAS_ON);
         g.setComposite(transparent);
 
-        //draw the contents of the JFrame's content pane upon our glass pane.
         contentPane.paint(gr);
         g.setColor(Color.white);
         g.fillRect(0, 0, getWidth(), getHeight());
@@ -54,7 +62,13 @@ public class GUIGlassPane extends JPanel implements ActionListener {
         g.drawString("test!", 50, 120);
     }
 
-    public void actionPerformed(ActionEvent ae) {
-        setVisible(false);
+    private void MakeEventListener() {
+        btnSave.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                setVisible(false);
+            }
+        });
+
     }
 }
--- Dots/src/be/kdg/dots/view/GUISpel.java
+++ Dots/src/be/kdg/dots/view/GUISpel.java
@@ -10,7 +10,7 @@ import java.awt.*;
  */
 public class GUISpel extends JPanel {
 
-    private JPanel gamePanel, gridSouth;
+    private JPanel gamePanel, gridSouth, flowLayout;
     private GUIGrid gridGame;
     private JLabel lblLevel, lblScore, lblTime;
     private JButton btnMenu;
@@ -29,21 +29,38 @@ public class GUISpel extends JPanel {
     private void MakeComponents() {
         btnMenu = new JButton("Menu");
         gridGame = new GUIGrid(controller);
+
+        ImageIcon iconLevel= new ImageIcon(getClass().getResource("/be/kdg/dots/resources/images/lblLevel.png"));
+        iconLevel = new ImageIcon(resize(iconLevel, 20,20));
         lblLevel = new JLabel("Level:");
+        lblLevel.setIcon(iconLevel);
+
+        ImageIcon iconScore= new ImageIcon(getClass().getResource("/be/kdg/dots/resources/images/lblScore.png"));
+        iconScore = new ImageIcon(resize(iconScore, 20,20));
         lblScore = new JLabel("Score:");
+        lblScore.setIcon(iconScore);
+
+        ImageIcon iconTime = new ImageIcon(getClass().getResource("/be/kdg/dots/resources/images/lblTime.png"));
+        iconTime = new ImageIcon(resize(iconTime, 20,20));
         lblTime = new JLabel("Time: 45");
+        lblTime.setIcon(iconTime);
+    }
+
+    public static Image resize(ImageIcon imageIcon, int width, int height) {
+        Image image = imageIcon.getImage();
+        Image newImg = image.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH);
+        return newImg;
     }
 
     private void MakeLayout() {
         gamePanel = new JPanel(new BorderLayout());
-        gridSouth = new JPanel(new GridLayout());
+        flowLayout = new JPanel(new FlowLayout(FlowLayout.CENTER, 70,10));
         gamePanel.setBackground(Color.white);
-        gridSouth.setBackground(Color.white);
         gridGame.setBackground(Color.white);
-        gridSouth.add(lblLevel);
-        gridSouth.add(lblScore);
-        gridSouth.add(lblTime);
-        gamePanel.add(gridSouth, BorderLayout.SOUTH);
+        flowLayout.add(lblLevel);
+        flowLayout.add(lblScore);
+        flowLayout.add(lblTime);
+        gamePanel.add(flowLayout, BorderLayout.SOUTH);
         gamePanel.add(gridGame, BorderLayout.CENTER);
         super.add(gamePanel);
 
