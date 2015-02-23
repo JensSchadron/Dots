c33c078b770dc1c4244f08e5a1e76f1e317637e8
Jens <jensschadron@hotmail.com>
2015-02-23 23:47:02 +0100

    2 februari 2015 - 23:46 Toevoegen about info gebruik makend van html 3.2 formatting

--- Dots/src/be/kdg/dots/view/GUIAboutPane.java
+++ Dots/src/be/kdg/dots/view/GUIAboutPane.java
@@ -11,24 +11,40 @@ import java.awt.event.MouseEvent;
 public class GUIAboutPane extends JPanel{
     private Container contentPane;
     private JButton btnClose;
+    private JLabel lblInfo;
 
     public GUIAboutPane(Container contentPane) {
         super();
         this.contentPane = contentPane;
-        setLayout(null);
+        setLayout(new BorderLayout());
+        //setLayout(null);
         MakeComponents();
         MakeLayout();
         MakeEventListener();
     }
 
     private void MakeComponents() {
+        lblInfo = new JLabel("<html>Hey daar.<p>" +
+                "<p>" +
+                "Welkom bij de info van ons spel.<p>" +
+                "Moest je het nog niet door hebben wat precies de bedoeling is van het spel, dan kan je hier alvast een " +
+                "deeltje van de uitleg vinden.<p>" +
+                "Dots is een spel waarbij gekleurde bolletjes, de zogenaamde dots, een zo lang mogelijke ketting moeten " +
+                "vormen. Er zijn wel twéé voorwaarden: De dots moeten aangrenzend zijn en ze moeten dezelfde kleur hebben.<p>" +
+                "Als je nog steeds niet goed door hebt hoe je het spel precies moet spelen, dan kan je nog steeds terecht bij " +
+                "de help die je bereikt door op de help-knop in het hoofdmenu te drukken.<p>" +
+                "<p>" +
+                "En dan nu een beetje info over onszelf.<p>" +
+                "Had je nu echt gedacht dat je nu al info over ons hier zou vinden? Het kan best zijn dat deze er nog komt op een later tijdstip ;)" +
+                "</html>");
         btnClose = new JButton("Close");
     }
 
     private void MakeLayout() {
+        add(lblInfo, BorderLayout.CENTER);
         btnClose.setForeground(Color.BLUE);
-        add(btnClose);
-        btnClose.setBounds(40, getHeight() / 2, 350, 30);
+        add(btnClose, BorderLayout.SOUTH);
+        //btnClose.setBounds(40, getHeight() / 2, 350, 30);
     }
 
     @Override
@@ -45,14 +61,16 @@ public class GUIAboutPane extends JPanel{
         g.setColor(Color.white);
         g.fillRect(0, 0, getWidth(), getHeight());
 
-        AlphaComposite solid = AlphaComposite.getInstance(AlphaComposite.SRC_OVER , 1f);
+        /*AlphaComposite solid = AlphaComposite.getInstance(AlphaComposite.SRC_OVER , 1f);
         g.setComposite(solid);
         g.setColor(Color.black);
         g.drawString("Hier komt de about info (Info over onszelf en het spel)", 50, 100);
-        g.drawString("test!", 50, 120);
+        g.drawString("test!", 50, 120);*/
     }
 
     private void MakeEventListener() {
+        this.addMouseListener(new MouseAdapter() {
+        });
         btnClose.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseReleased(MouseEvent e) {
